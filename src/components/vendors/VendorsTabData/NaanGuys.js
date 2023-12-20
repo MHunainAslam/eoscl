8e36f7a89caa3d18e2f8d9515be6f5ee0ea9a0cd
mhunainaslam <hunainaslam.ha@gmail.com>
2023-12-21 03:33:41 +0500

    .

--- src/components/vendors/VendorsTabData/NaanGuys.js
+++ src/components/vendors/VendorsTabData/NaanGuys.js
@@ -31,15 +31,19 @@ const NaanGuys = ({ tabdata }) => {
                 setisLoading(false)
             });
     }, [])
-    const router = useSearchParams();
-    // const { image } = router.query;
-    console.log(router, 'lololo');
+    const searchParams = useSearchParams()
+
+    const logo = JSON.parse(searchParams.get('state'))
+    console.log(logo?.url);
+
 
 
 
     return (
 
         <>
+
+            {/* {parsedState && <p>{parsedState}ddd</p>} */}
             <section className='py-4'>
                 <div className="container">
                     <div className="row">
@@ -51,16 +55,16 @@ const NaanGuys = ({ tabdata }) => {
                                     <div className=" col-lg-4 col-md-6 mt-3 ">
                                         <div className="card vendor-card h-100">
                                             <div className="card-header">
-                                                {item.image === null ?
+                                                {logo === null ?
                                                     <Image src={'/assets/images/download.png'} className='pt-2' width={1000} height={1000} alt=''></Image> :
-                                                    <Image loader={imgurl} src={logo?.image?.url} className='pt-2' width={1000} height={1000} alt=''></Image>
+                                                    <Image loader={imgurl} src={logo?.url} className='pt-2' width={1000} height={1000} alt=''></Image>
                                                 }
                                                 <div className="discount p-3">
                                                     <div className="heading-m text-white text-center">{item.discount}</div>
                                                     <div className="heading-m text-white text-center">% Off</div>
 
                                                 </div>
-                                                
+
                                             </div>
                                             <div className="card-body">
                                                 <p className="para">{item.description} </p>
